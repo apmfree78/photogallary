efac596179e802ab9365a374d9ce43bd1dc98ee2
apmfree78 <amit@profitswami.com>
2022-07-26 18:10:06 -0400

    likes reducer up and working plus single photo view

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
 import { Link } from 'react-router';
+// import comments
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    return <div className='single-photo'>single photo</div>;
+    // index of post
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+    return (
+      <div className='single-photo'>
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
+      </div>
+    );
   },
 });
 
