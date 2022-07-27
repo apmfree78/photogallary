30e8232b6b190963cbb2e7253a434d3da1075bc5
apmfree78 <amit@profitswami.com>
2022-07-27 12:22:50 -0400

    post comments reducer all set

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -3,8 +3,30 @@
 // 1. the action (info of what happens)
 // 2. copy of current state
 
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment,
+        },
+      ];
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
-  console.log('comment');
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action),
+    };
+  }
   return state;
 }
 
